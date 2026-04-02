ae0cf4d834b9562ce71a485079571f63af3de69e
Daniel Fan <danielfan@Mac-Studio.local>
2026-04-02 10:21:52 -0400

    rename components to .jsx

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.jsx', '.js', '.tsx', '.ts']
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001'
